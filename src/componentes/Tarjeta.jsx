ff4e707f0897376354a0ab2daf56ad04609745a4
Fabian547 <szadorskifabian@gmail.com>
2025-06-24 00:00:01 -0300

    trabajo terminado ecepto css

--- src/componentes/Tarjeta.jsx
+++ src/componentes/Tarjeta.jsx
@@ -1,17 +1,16 @@
-import { useState } from 'react';
-
-export default function Tarjeta({ nombre, categoria, prioridad, estado: estadoInicial, eliminar }) {
-  const [estado, setEstado] = useState(estadoInicial);
-
+export default function Tarjeta({nombre,categoria,prioridad,estado,eliminar,onCambiarEstado,}) 
+{
+  console.log("categoria:", categoria);
+console.log("prioridad:", prioridad);
   return (
-    <div className="Tarjeta" style={{ backgroundColor: "lightblue" }}>
+    <div className="Tarjeta" style={{ backgroundColor: "lightblue", margin: "1rem", padding: "1rem" }}>
       <span className="Eliminar" onClick={eliminar}>X</span>
       <span>Nombre: {nombre}</span>
       <span>Categoría: {categoria}</span>
       <span>Prioridad: {prioridad}</span>
       <span>
         Estado:
-        <select value={estado} onChange={(e) => setEstado(e.target.value)}>
+        <select value={estado} onChange={(e) => onCambiarEstado(e.target.value)}>
           <option value="Pendiente">Pendiente</option>
           <option value="En proceso">En proceso</option>
           <option value="Finalizado">Finalizado</option>
@@ -19,4 +18,5 @@ export default function Tarjeta({ nombre, categoria, prioridad, estado: estadoIn
       </span>
     </div>
   );
+  
 }
